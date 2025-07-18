f64a0c18d3b39403b19bb213f1c1fe7d3376fde1
dearguru07 <pangalaguruyadav101@gmail.com>
2025-07-18 06:28:38 +0530

    digitscount

--- app.js
+++ app.js
@@ -194,3 +194,24 @@ for (let i = 0; i < n; i++){
     }
     console.log(row)
 }
+
+
+function Polyndrome(n) {
+    if(n<0)return false
+    let copyN=n
+    let rev=0
+    while (n > 0) {
+        let rem = n % 10;
+        rev = (10 * rev) + rem;
+        n=Math.floor(n/10)
+    }
+    // if (rev == copyN) {
+    //     return true
+    // }
+    // else {
+    //     return false
+    // }
+    return (rev===copyN)
+}
+let res = Polyndrome(14551)
+console.log(res)
