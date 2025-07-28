8efa5ff2693c5ecdc2fd36c591d4ee1a0271a3ef
dearguru07 <pangalaguruyadav101@gmail.com>
2025-07-28 20:05:29 +0530

    new

--- index.js
+++ index.js
@@ -318,15 +318,23 @@
 // console.log(res)
 
 
-for (let i = 0; i < 3; i++){
-    for (let j = i; j < 0; j--){
-        console.log(i)
-    }
-}
+// for (let i = 0; i < 3; i++){
+//     for (let j = i; j < 0; j--){
+//         console.log(i)
+//     }
+// }
 
 
-for (let i = 5; i > 0; i--){
-    for (let j = 0; j < i; j++){
-        console.log(i,j)
+// for (let i = 5; i > 0; i--){
+//     for (let j = 0; j < i; j++){
+//         console.log(i,j)
+//     }
+// }
+
+for(let i=0;i<5;i++){
+    let row='';
+    for(let j=0;j<5;j++){
+        row=row+' *'
     }
+    console.log(row)
 }
