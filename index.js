65f2383c9d78e964a76121126e62691d95846c21
dearguru07 <pangalaguruyadav101@gmail.com>
2025-07-31 15:19:50 +0530

    c

--- index.js
+++ index.js
@@ -338,3 +338,24 @@ for(let i=0;i<5;i++){
     }
     console.log(row)
 }
+
+
+
+let n=5
+
+for (let i = 0; i <n; i++){
+    let row = '';
+    for (let j = 0; j < n - i; j++){
+        row=row+(j+1)
+    }
+    console.log(row)
+}
+
+
+for (let i = 0; i < n; i++) {
+    let row = '';
+    for (let j = 0; j < n - i; j++) {
+        row = row + ('*')
+    }
+    console.log(row)
+}
