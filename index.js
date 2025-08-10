2b44c5042f5453d319189a92bc4cdf6d8531204a
dearguru07 <pangalaguruyadav101@gmail.com>
2025-08-10 14:12:40 +0530

    donee

--- index.js
+++ index.js
@@ -363,36 +363,36 @@
 
 
 
-let n=5
-for (let i = 0; i < n; i++){
-    let row = ''
-    for (let j = 0; j < n - (i + 1); j++){
-        row=row+' '
-    }
-    for (let k = 0; k < (i + 1); k++){
-        row=row+'*'
-    }
-    console.log(row)
-}
+// let n=5
+// for (let i = 0; i < n; i++){
+//     let row = ''
+//     for (let j = 0; j < n - (i + 1); j++){
+//         row=row+' '
+//     }
+//     for (let k = 0; k < (i + 1); k++){
+//         row=row+'*'
+//     }
+//     console.log(row)
+// }
 
 
 
 
 
-for (let i = 0; i < n; i++){
-    let row = '';
-    let toggle = 1;
-    for (let j = 0; j <= i; j++){
-        row = row + toggle;
-        if (toggle == 0) {
-            toggle=1
-        }
-        else {
-            toggle=0
-        }
-    }
-    console.log(row)
-}
+// for (let i = 0; i < n; i++){
+//     let row = '';
+//     let toggle = 1;
+//     for (let j = 0; j <= i; j++){
+//         row = row + toggle;
+//         if (toggle == 0) {
+//             toggle=1
+//         }
+//         else {
+//             toggle=0
+//         }
+//     }
+//     console.log(row)
+// }
 
 
 
@@ -507,3 +507,17 @@ for (let i = 0; i < n; i++){
 // console.log(res)
 
 
+var removeDuplicates = function(nums) {
+    let c=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>nums[c]){
+            c=c+1
+            nums[c]=nums[i]
+        }
+    }
+    return c+1
+};
+
+let nums=[1,2,2,3,3,4,4,4,5,2];
+// removeDuplicates(nums)
+console.log(removeDuplicates(nums))
