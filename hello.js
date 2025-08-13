3a5b25f4c0759eb5657eb4bee1ee049e35b3709b
dearguru07 <pangalaguruyadav101@gmail.com>
2025-08-13 19:32:44 +0530

    done

--- hello.js
+++ hello.js
@@ -836,3 +836,100 @@ for (let i = 0; i < n; i++){
     }
     console.log(row)
 }
+for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j < i + 1; j++){
+        row=row+'x'
+    }
+    console.log(row)
+}v
