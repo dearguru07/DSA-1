547b00734c4699a59fd9f001aaad45d1a2a6be6f
dearguru07 <pangalaguruyadav101@gmail.com>
2025-08-29 22:12:43 +0530

    d

--- section.js
+++ section.js
@@ -8493,3 +8493,13 @@ for (let i = 0;i < n; i++) {
     }
     console.log(row)
 }
+
+
+
+for (let i = 0; i < n; i++){
+    let row = '';
+    for (let j = 0; j <= n - i - 1; j++){
+        row = row + 'x';
+    }
+    console.log(row)
+}
